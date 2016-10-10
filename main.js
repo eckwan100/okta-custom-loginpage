0a6b57415cbb5d6d5a7d6adf74e86b0aa69c56a5
Edwin Kwan <edwinkwan@ML-EKWAN-OSX-SF.local>
2016-10-09 21:27:00 -0700

    First Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type' : 'text/html'});
+	res.end('<h1>Hello</h1>');
+
+});
+
+server.listen(3000);
