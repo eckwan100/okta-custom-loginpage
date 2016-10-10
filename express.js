e0582b14cb7b4db2072184f8eeffd0a0a594237a
Edwin Kwan <edwinkwan@ML-EKWAN-OSX-SF.local>
2016-10-09 23:28:38 -0700

    added express

--- express.js
+++ express.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+//app.set('port', (process.env.PORT || 5000));
 
 //app.use(express.static(__dirname + '/public'));
 
@@ -9,10 +16,12 @@ app.set('port', (process.env.PORT || 5000));
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.send('hi');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
