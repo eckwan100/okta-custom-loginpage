172007075e11e1ab83b23fdf8e2eff16362c8092
Edwin Kwan <edwinkwan@ML-EKWAN-OSX-SF.local>
2016-10-10 00:18:52 -0700

    added express

--- express.js
+++ express.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-
-// Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,11 +13,11 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
-app.post('/process_post', urlencodedParser, function (req, res) {
+app.get('/process_get', function (req, res) {
    // Prepare output in JSON format
    response = {
-      first_name:req.body.first_name,
-      last_name:req.body.last_name
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
    };
    console.log(response);
    res.end(JSON.stringify(response));
