b5cc169a498f5ed8d7f036f7f15cd54d14e2959c
Edwin Kwan <edwinkwan@ML-EKWAN-OSX-SF.local>
2016-10-09 23:58:57 -0700

    added express

--- express.js
+++ express.js
@@ -9,9 +9,19 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('index');
-});
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
