2a2eaeaf751a2638ebc5169a6355a3cdd8061400
Edwin Kwan <edwinkwan@ML-EKWAN-OSX-SF.local>
2016-10-09 23:39:23 -0700

    added express

--- express.js
+++ express.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
+// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('index');
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
