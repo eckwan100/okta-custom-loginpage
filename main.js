d9fa4da0d62373de0c301e6a11a9d11f04ee62f1
Edwin Kwan <edwinkwan@ML-EKWAN-OSX-SF.local>
2016-10-13 21:43:19 -0700

    Another commit

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type' : 'text/html'});
-	res.end('<h1>Hello</h1>');
+app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  //response.render('index');
+  response.render('index');
 });
 
-var port = Number(process.env.PORT || 3000);
+/*app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      username:req.query.username,
+      password:req.query.password
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})*/
 
-server.listen(port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
